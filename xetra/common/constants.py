7a4b3cc8942df531bc92befcf78f3aeb3f1f6114
slowikr <radek.slowik@gmail.com>
2022-09-14 05:06:28 +0200

    commit for code implementation

---
+++ xetra/common/constants.py
@@ -0,0 +1,23 @@
+"""
+File to store constains
+"""
+from enum import Enum
+
+class S3FileTypes(Enum):
+    """
+    Supported files types for S3BucketConnector
+    """
+
+    csv = "csv"
+    PARQUET = "parquet"
+
+class MetaProcessFormat(Enum):
+    """
+    Format for MetadataProcess class
+    """
+
+    META_DATE_FORMAT='%Y-%m-%d'
+    META_PROCESS_DATE_FORMAT='%Y-%m-%d %H:%M:%S'
+    META_SOURCE_DATE_COL = 'source_date'
+    META_PROCESS_COL= 'datetime_of_processing'
+    META_FILE_FORMAT = 'csv'
---
+++ xetra/common/s3.py
@@ -0,0 +1,29 @@
+"""Connector and methods accessing S3"""
+import os
+import boto3
+class S3BucketConnector():
+    """
+    CLass for interacting with S3 Buckets
+    """
+
+    def __init__(self, access_key: str, secret_key: str, endpoint_url:str, bucket: str):
+        """
+        Constructor for S3BucketConnector
+        :param access_key: access key for connecting with S3
+        :param secret_key: secret key for connecting with S3
+        :param endpoint_url: endpoint url to s3
+        :param bucket: S3 Bucket
+        """
+        self.endpoint_url=endpoint_url
+        self.session = boto3.Session(aws_access_key_id=os.environ[access_key], aws_secret_access_key=os.environ[secret_key])
+        self._s3=self.session.resource(service_name='s3', endpoint_url=endpoint_url)
+        self._bucket=self._s3.Bucket(bucket)
+
+    def list_in_prefix(self):
+        pass
+
+    def read_csv_to_df(self):
+        pass
+
+    def write_df_to_s3(self):
+        pass
